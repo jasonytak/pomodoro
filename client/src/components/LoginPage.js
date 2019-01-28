f8610bd10bd77a7e32ec76afdb8749ca4e954d05
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-01-28 15:58:34 -0800

    Added basic login-page form

---
+++ client/src/components/LoginPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class LoginPage extends React.Component {
+  state = { name: '' };
+
+  render() {
+    return (
+      <div className="ui one column stackable center aligned page grid">
+        <div className="column twelve wide">
+          <form className="ui form">
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;

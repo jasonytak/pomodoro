80491872dc1e9a3db74cc6d848ed409f926d3415
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-01-28 16:23:04 -0800

    Added text inputs and labels

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,3 +1,4 @@
+import './LoginPage.css';
 import React from 'react';
 
 class LoginPage extends React.Component {
@@ -5,19 +6,24 @@ class LoginPage extends React.Component {
 
   render() {
     return (
-      <div className="ui one column stackable center aligned page grid">
-        <div className="column twelve wide">
-          <form className="ui form">
-            <div className="field">
-              <label>Name</label>
-              <input
-                type="text"
-                value={this.state.name}
-                onChange={e => this.setState({ name: e.target.value })}
-              />
-            </div>
-            <button className="ui button" type="submit">Submit</button>
-          </form>
+      <div className="login-container">
+        <div className="ui one column stackable center aligned page grid">
+          <h1 className="ui header">PAY ATTENTION</h1>
+          <div className="column twelve wide">
+            <form className="ui segment form">
+              <div className="field">
+                <label>Name</label>
+                <input
+                  type="text"
+                  value={this.state.name}
+                  onChange={e => this.setState({ name: e.target.value })}
+                />
+              </div>
+              <button className="ui button" type="submit">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     );

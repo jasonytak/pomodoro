8fde0b0ab83a164665e33f8fd1aa214b859304ea
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-01-29 15:10:34 -0800

    Modularized mongoose controller

--- models/userController.js
+++ models/userController.js
@@ -1,10 +1,11 @@
-const User = require('./User.js');
+const User = require('./User');
 
 const userController = {};
 
 userController.saveUser = (req, res, next) => {
   User.create({ user: req.body.user }, (err, response) => {
     if (err) throw err;
+    console.log(response);
     res.send(response);
   });
 };

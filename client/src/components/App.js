283eaea83f06b65d3212ba43a19116775ea47eb6
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-01-29 15:37:49 -0800

    Revised axios method to set state after data entry

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,25 +3,33 @@ import LoginPage from './LoginPage';
 import axios from 'axios';
 
 class App extends React.Component {
+  state = { users: [] };
+  // componentDidMount() {
+  //   this.callExpress();
+  // }
 
+  // callExpress = async () => {
+  //   const response = await fetch('/server');
+  //   const body = await response.json();
+  //   console.log(body);
+  //   return body;
+  // };
 
-  componentDidMount() {
-    this.callExpress();
-  }
-
-  callExpress = async () => {
-    const response = await fetch('/server');
-    const body = await response.json();
-    console.log(body);
-    return body;
-  }
-
-  onSubmit = (user) => {
-    axios.post('/user', { user });
-  }
+  onSubmit = user => {
+    axios.post('/user', { user }).then(res =>
+      this.setState(state => {
+        const users = this.state.users.concat(res.data.user);
+        return { users };
+      })
+    );
+  };
 
   render() {
-    return <div><LoginPage onSubmit={this.onSubmit} /></div>;
+    return (
+      <div>
+        <LoginPage onSubmit={this.onSubmit} />
+      </div>
+    );
   }
 }
 

387bf86c60b57cdfe877f9ae2aa49aa113f56982
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-01-29 15:06:06 -0800

    Created route that connects to mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const userController = require('./models/userController');
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+const mongoDB = 'mongodb://test:test123@ds145563.mlab.com:45563/pomodoro';
+
+mongoose.connect(mongoDB).then(() => console.log('CONNECTED'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/user', userController.saveUser);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 

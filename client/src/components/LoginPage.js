64e9d2db4db0d8e09d9ac6f73f537d7e234cfc16
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-01-28 16:04:17 -0800

    Added name input field and submit button

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -8,6 +8,15 @@ class LoginPage extends React.Component {
       <div className="ui one column stackable center aligned page grid">
         <div className="column twelve wide">
           <form className="ui form">
+            <div className="field">
+              <label>Name</label>
+              <input
+                type="text"
+                value={this.state.name}
+                onChange={e => this.setState({ name: e.target.value })}
+              />
+            </div>
+            <button className="ui button" type="submit">Submit</button>
           </form>
         </div>
       </div>

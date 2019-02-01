f0cf0c73977115e6ba2f32ad151befa9c3ac0375
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-01-31 20:10:25 -0800

    Added additional route for multiple queries

--- models/userController.js
+++ models/userController.js
@@ -2,15 +2,23 @@ const User = require('./User');
 
 const userController = {};
 
-userController.saveUser = (req, res, next) => {
-  User.create({ user: req.body.user }, (err, response) => {
-    if (err) throw err;
-    res.send(response);
-  });
-};
+// userController.saveUser = (req, res, next) => {
+//   User.create({ user: req.body.user }, (err, response) => {
+//     if (err) throw err;
+//     res.send(response);
+//   });
+// };
 
-userController.findUsers = (req, res, next) => {
-  User.find({}, (err, user) => {
+// userController.findUsers = (req, res, next) => {
+//   User.find({}, (err, user) => {
+//     if (err) throw err;
+//     res.send(user);
+//   });
+// };
+
+userController.saveAndFind = async (req, res, next) => {
+  await User.create({ user: req.body.user });
+  await User.find({}, (err, user) => {
     if (err) throw err;
     res.send(user);
   });

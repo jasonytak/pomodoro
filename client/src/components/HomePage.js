e4aad2ed3b87715c5ca02991588bf417bd80ab3e
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-01-31 20:24:47 -0800

    Added es6 - deconstructuring

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import UserCard from './UserCard';
+import CountdownClock from './CountdownClock';
+
+// class HomePage extends React.Component {
+
+//   render() {
+//     return (
+//       <div>
+//         <div>Hello</div>
+//       </div>
+//     );
+//   }
+// }
 
 const HomePage = props => {
-  const users = props.users.map(user => {
-    return (
-      <UserCard user={user} />
-    );
+  const users = props.users.map(({user}) => {
+    console.log(user);
+    return <UserCard user={user} />;
   });
-  return <div>{users}</div>;
+  return (
+    <div>
+      <CountdownClock />
+      <div className="ui link cards">{users}</div>
+    </div>
+  );
 };
 
 export default HomePage;

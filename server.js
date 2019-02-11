0419f358e1e7c172b68bf3aa04b41e0a20abbd2a
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-02-11 14:00:13 -0800

    Heroku deployment

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build'));
 });
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));

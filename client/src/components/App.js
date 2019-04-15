ab1412248a9e43090613efd18b5a10345dc2244d
Jason Tak <jasonytak@gmail.com>
2019-04-14 22:33:21 -0600

    Revised app component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,29 @@
 import './App.css';
 import React from 'react';
-import Menu from './Menu';
 import PomodoroDisplay from './PomodoroDisplay';
 import PomodoroButtons from './PomodoroButtons';
 import PomodoroInstructions from './PomodoroInstructions';
+import InputModal from './InputModal';
 
 class App extends React.Component {
-  state = { totalSeconds: 1500, isRunning: false };
+  state = {
+    totalSeconds: null,
+    totalWorkMinutes: 1,
+    totalBreakMinutes: 2,
+    isRunning: false,
+    currentState: 'work'
+  };
 
   onStart = () => {
-    this.setState({ isRunning: true });
+    this.setState({
+      totalSeconds: this.state.totalWorkMinutes * 60,
+      isRunning: true
+    });
     this.interval = setInterval(() => this.tick(), 1000);
   };
 
   onReset = () => {
-    this.setState({ totalSeconds: 1500 });
+    this.setState({ totalSeconds: this.state.totalWorkMinutes * 60 });
   };
 
   onPause = () => {
@@ -29,10 +38,41 @@ class App extends React.Component {
     this.setState({ totalSeconds: this.state.totalSeconds - 1 });
   };
 
+  switchTimer = () => {
+    if (this.state.currentState === 'work') {
+      this.setState({
+        totalSeconds: this.state.totalBreakMinutes * 60,
+        currentState: 'break'
+      });
+    } else if (this.state.currentState === 'break') {
+      this.setState({
+        totalSeconds: this.state.totalWorkMinutes * 60,
+        currentState: 'work'
+      });
+    }
+  };
+
+  onInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
       <div className="pomodoro-app">
-        <Menu />
+        <div className="pomodoro-menu">
+          <h1>Pomodoro Timer</h1>
+          <div className="pomodoro-menu-buttons">
+            <InputModal
+              onInputChange={this.onInputChange}
+              handleSubmit={this.handleSubmit}
+              totalWorkMinutes={this.state.totalWorkMinutes}
+              totalBreakMinutes={this.state.totalBreakMinutes}
+            />
+            <button className="ui button">
+              <span>What is this?</span>
+            </button>
+          </div>
+        </div>
         <PomodoroDisplay totalSeconds={this.state.totalSeconds} />
         <PomodoroButtons
           onStart={this.onStart}

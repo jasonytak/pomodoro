b5c1385a238c59721abb2a8e8649981a49d97f5f
Jason Tak <jasonytak@gmail.com>
2019-04-14 22:33:00 -0600

    Added controlled inputs

--- client/src/components/InputModal.js
+++ client/src/components/InputModal.js
@@ -4,7 +4,7 @@ import { Button, Modal, Input } from 'semantic-ui-react';
 class InputModal extends React.Component {
   state = {
     modalOpen: false
-  }
+  };
 
   handleOpen = () => {
     this.setState({ modalOpen: true });
@@ -17,38 +17,40 @@ class InputModal extends React.Component {
   render() {
     return (
       <Modal
-      trigger={<Button onClick={this.handleOpen}>Customize Timers</Button>}
-      open={this.state.modalOpen}
-      onClose={this.handleClose}
-    >
-      <Modal.Header>Customize Timers</Modal.Header>
-      <Modal.Content>
-        <Input
-          name="customWork"
-          onChange={this.props.onInputChange}
-          placeholder="Enter Work Minutes..."
-          onKeyPress={(e) => allowNumbersOnly(e) }
-        />
-        <Input
-          name="customBreak"
-          onChange={this.onInputChange}
-          placeholder="Enter Break Minutes..."
-          onKeyPress={(e) => allowNumbersOnly(e) }
-        />
-      </Modal.Content>
-      <Modal.Actions>
-        <Button onClick={this.handleClose}>Submit</Button>
-      </Modal.Actions>
-    </Modal>
-    )
+        trigger={<Button onClick={this.handleOpen}>Customize Timers</Button>}
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+      >
+        <Modal.Header>Customize Timers</Modal.Header>
+        <Modal.Content>
+          <Input
+            name="totalWorkMinutes"
+            onChange={this.props.onInputChange}
+            placeholder="Enter Work Minutes..."
+            onKeyPress={e => allowNumbersOnly(e)}
+            value={this.props.totalWorkMinutes}
+          />
+          <Input
+            name="totalBreakMinutes"
+            onChange={this.props.onInputChange}
+            placeholder="Enter Break Minutes..."
+            onKeyPress={e => allowNumbersOnly(e)}
+            value={this.props.totalBreakMinutes}
+          />
+        </Modal.Content>
+        <Modal.Actions>
+          <Button onClick={this.handleClose}>Submit</Button>
+        </Modal.Actions>
+      </Modal>
+    );
   }
 }
 
-const allowNumbersOnly = (e) => {
-  let code = (e.which) ? e.which : e.keyCode;
+const allowNumbersOnly = e => {
+  let code = e.which ? e.which : e.keyCode;
   if (code > 31 && (code < 48 || code > 57)) {
-      e.preventDefault();
+    e.preventDefault();
   }
-}
+};
 
 export default InputModal;
